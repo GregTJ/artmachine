e51f1efd66e172985590d45ed70bc5c8825eb878
Gregory Johnson <gregorytj@outlook.com>
2019-12-04 18:13:19 -0700

    Fixed base.html title tag.

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'placeholder')
     GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
     GOOGLE_CSE_CX = os.environ.get('GOOGLE_CSE_CX')
-    EFFECT_COUNT = 3
-    NOUN_COUNT = 2
+    EFFECT_COUNT = int(os.environ.get('EFFECT_COUNT', 3))
+    NOUN_COUNT = int(os.environ.get('NOUN_COUNT', 2))
     TRAP_HTTP_EXCEPTIONS = True
     TEMPLATES_AUTO_RELOAD = True
