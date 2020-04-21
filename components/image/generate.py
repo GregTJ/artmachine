1df2cba7b246c18c91c5cfa0c7ba206a3782e08a
Gregory Johnson <gregorytj@outlook.com>
2020-04-20 22:21:19 -0600

    .

--- components/image/generate.py
+++ components/image/generate.py
@@ -21,7 +21,6 @@ def search(api_key: str, cse_cx: str, query: str, count: int = 10) -> set:
                   'q': query,
                   'searchType': 'image',
                   'fileType': 'png',
-                  'imgSize': 'XLARGE',
                   'imgType': 'photo',
                   'imgColorType': 'color',
                   'safe': 'off'}
